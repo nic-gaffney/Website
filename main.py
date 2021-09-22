a12ea15563bfdd048c43270107c350aeb89463c4
Gaffclant <Gaffclant@gmail.com>
2021-09-21 23:21:56 -0500

    :art: templating

--- main.py
+++ main.py
@@ -2,12 +2,21 @@
 import os.path
 
 import cherrypy
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(loader=FileSystemLoader('public'), autoescape=select_autoescape())
 
 
 class Website(object):
     @cherrypy.expose
     def index(self):
-        return open("public/html/index.html")
+        template = env.get_template("html/index.html")
+        return template.render(index=True)
+
+    @cherrypy.expose
+    def about(self):
+        template = env.get_template("html/about.html")
+        return template.render(about=True)
 
 
 if __name__ == '__main__':
