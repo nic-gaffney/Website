993ffc887a3e6471b347ed735fb0375587b79012
Gaffclant <Gaffclant@gmail.com>
2021-09-22 12:45:27 -0500

    :card_file_box: Added a databse for the blog

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 # Source code for my website gaffclant.com
+import datetime
 import os.path
 
 import cherrypy
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from peewee import *
 
 env = Environment(loader=FileSystemLoader('public'), autoescape=select_autoescape())
+db = SqliteDatabase('blog_database.db')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Post(BaseModel):
+    created_date = DateTimeField(default=datetime.datetime.now)
+    title = TextField()
+    text = TextField()
+    id = IntegerField(unique=True)
+
+
+db.connect()
+db.create_tables([Post])
 
 
 class Website(object):
     @cherrypy.expose
     def index(self):
         template = env.get_template("html/index.html")
-        return template.render(index=True)
+        return template.render(index=True, blog=["TEST", "THIS NO SHOW"])
 
     @cherrypy.expose
     def about(self):
