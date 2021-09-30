42799b1ff7a51a4eb53e2e9e1f6c72943252cc4d
Gaffclant <65436210+Gaffclant@users.noreply.github.com>
2021-09-30 08:33:11 -0500

    :bug: Probably still broken

--- main.py
+++ main.py
@@ -99,6 +99,10 @@ USERS = json.loads(USERS)
 
 if __name__ == '__main__':
     conf = {
+        'global': {
+            'server.socket_host': '0.0.0.0',
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
+        },
         '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
